18160b49cd311151d3399df87cb558f0521a42f3
Vladimir Urkov <snegovikkvolodya@yandex.ru>
2024-09-29 17:11:42 +0300

    add preloader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import About from "./About";
 import Face from "./Basic";
 import Footer from "./Footer";
@@ -5,14 +6,35 @@ import Nav from "./Nav";
 import Projects from "./Projects";
 
 
+function PreLoader() {
+    return (
+        <div className="preloader-container">
+            <div className="preloader">
+                <div className="circle circle1"></div>
+                <div className="circle circle2"></div>
+                <div className="circle circle3"></div>
+                <div className="circle circle4"></div>
+            </div>
+        </div>
+    );
+  }
+
 export default function App() {
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        setTimeout(() => {
+          setLoading(false);
+        }, 1500);
+    }, []);
     return (
         <>
-            <Face/>
-            <Nav/>
-            <About/>
-            <Projects/>
-            <Footer/>
+            {loading ? <PreLoader/> : [
+                <Face/>,
+                <Nav/>,
+                <About/>,
+                <Projects/>,
+                <Footer/>
+            ]}
         </>
     )
 }
